6313c8ef7f5294a87ed71ddcc8790f0a1238bea7
trietng <93473943+trietng@users.noreply.github.com>
2022-06-30 22:55:49 +0700

    Update dictionary.hpp
    +) Rename filename to filepath
    +) Re-introduce text-file reading as read_text()

--- dictionary.hpp
+++ dictionary.hpp
@@ -10,8 +10,8 @@ class dictionary {
 private:
 	trie<N_WORD> word;
 	trie<N_DEF> definition;
-	std::string filename;
-	//Private recursive write to text file function
+	std::string filepath;
+	//Write to text file
 	void write_text(tnode<N_WORD>* root, std::ofstream& fout) {
 		if (!root) return;
 		if (!root->value.empty()) {
@@ -22,7 +22,7 @@ private:
 			write_text(root->next[i], fout);
 		}
 	}
-	//Read trie from file
+	//Read trie from binary file
 	void read(std::ifstream& fin, tnode<N_WORD>*& node) {
 		char ch = '\0';
 		fin.read((char*)&(ch), sizeof(char));
@@ -50,7 +50,7 @@ private:
 			}
 		}
 	}
-	//Write trie to file
+	//Write trie to binary file
 	void write(std::ofstream& fout, tnode<N_WORD>* node) {
 		if (!node) fout.write((char*)&(grave_accent), sizeof(char));
 		else {
@@ -73,25 +73,40 @@ private:
 		}
 	}
 public:
-	dictionary(const std::string& filename) {
-		this->filename = filename;
-		std::ifstream fin(filename, std::ios::binary);
+	dictionary(const std::string& filepath) {
+		this->filepath = filepath;
+		std::ifstream fin(filepath, std::ios::binary);
 		if (fin) {
 			for (unsigned int i = 0; i < N_WORD; ++i) {
 				read(fin, word.top()->next[i]);
 			}
 		}
-		else std::cout << "ERROR: BAD FILE AT " << filename;
+		else std::cout << "ERROR: BAD FILE AT " << filepath;
 	}
-	//Write trie to file
+	//Read from text file
+	void read_text(const std::string& filepath) {
+		clear();
+		std::ifstream fin(filepath);
+		this->filepath = filepath;
+		if (fin) {
+			std::string line, word, def;
+			while (getline(fin, line)) {
+				std::stringstream ss(line);
+				std::getline(ss, word, '`');
+				std::getline(ss, def);
+				insert(word, def);
+			}
+		}
+	}
+	//Write trie to binary file
 	void write() {
-		std::ofstream fout(filename, std::ios::binary);
+		std::ofstream fout(filepath, std::ios::binary);
 		if (fout) {
 			for (unsigned int i = 0; i < N_WORD; ++i) {
 				write(fout, word.top()->next[i]);
 			}
 		}
-		else std::cout << "ERROR: BAD FILE AT " << filename;
+		else std::cout << "ERROR: BAD FILE AT " << filepath;
 	}
 	void insert(const std::string& word, const std::string& definition) {
 		shptr<entry> ent(new entry(word, definition));
@@ -112,11 +127,11 @@ public:
 	std::vector<entry*> find_definition(const std::string& keyword) {
 		return this->definition.find_d(keyword);
 	}
-
+	//Write to text file
 	void write_text() {
-		std :: ofstream fout(filename + ".txt");
+		std :: ofstream fout(filepath + ".txt");
 		fout.close();
-		fout.open(filename + ".txt", std::ios::app);
+		fout.open(filepath + ".txt", std::ios::app);
 		if (fout) write_text(word.top(), fout);
 	}
 
@@ -127,12 +142,13 @@ public:
 	void clear() {
 		word.clear();
 		definition.clear();
+		filepath.clear();
 	}
 
 	void restore() {
+		std::string t_filepath = filepath;
 		clear();
-		std::string temp = "data\\backup\\" + std::filesystem::path(filename).filename().string(); // ???
+		std::string temp = "data\\backup\\" + std::filesystem::path(t_filepath).filename().string(); // ???
 		dictionary(temp);
 	}
-	
 };
