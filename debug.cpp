56356accc403e68cacc5eb7258e6a9df1e779751
trietng <93473943+trietng@users.noreply.github.com>
2022-07-14 23:04:08 +0700

    add show_console()
    Call this function to open std console for debugging and other things. It is located in the header debug.h. Should be removed from the project on release.

---
+++ debug.cpp
@@ -0,0 +1,9 @@
+#include "debug.h"
+
+
+void show_console() {
+    AllocConsole();
+    freopen("conin$", "r", stdin);
+    freopen("conout$", "w", stdout);
+    freopen("conout$", "w", stderr);
+}
---
+++ debug.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <cstdio>
+// For debugging only
+#define WIN32_LEAN_AND_MEAN
+#include "Windows.h"
+
+void show_console();
